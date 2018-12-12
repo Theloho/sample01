95eb780be3f489e75cc958041966e8a08acd23ce
Jintae Kim <>
2018-12-12 18:30:00 +0900

    Updates

---
+++ precache-manifest.934e4e6507ef75f2bf182714276fd121.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "e6d56b5748fcc52e545b",
+    "url": "/movie-app/static/js/main.e6d56b57.chunk.js"
+  },
+  {
+    "revision": "eab4cdaf67fac56c6ac5",
+    "url": "/movie-app/static/js/1.eab4cdaf.chunk.js"
+  },
+  {
+    "revision": "e6d56b5748fcc52e545b",
+    "url": "/movie-app/static/css/main.0ecfe49f.chunk.css"
+  },
+  {
+    "revision": "4874cf6345b73833e176f0b6c4839511",
+    "url": "/movie-app/index.html"
+  }
+];
